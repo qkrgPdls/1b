503c5678b8066ba75ebabcaa80dd96a9cf8523b2
hacktrole <108964372+hacktrole@users.noreply.github.com>
2022-07-09 17:50:16 +0900

    0 10773

--- 이름.cpp
+++ 이름.cpp
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main (){
-	printf("hello");
+	int i, n;
+	scanf("%d", &n);
+	int* aa = (int*)malloc(4*n);
+	for(i = 0;i < n;i++) scanf("%d", &aa[i]);
+	int a = aa[0];
+	for(i = 1;i < n;i++){
+		if(aa[i] != 0) a += aa[i];
+		else {
+			for(int j = 1;j < n;j++){
+				if(aa[i-j] != 0) {
+					
+					a -= aa[i-j];
+					break;
+				}
+			}
+		}
+		
+		printf("%d   ", a);
+	}
+	printf("%d", a);
 	return 0;
 }
